f957a5e53e241583a1ee5d28c580dd3b988ed573
Rajmund Zawislak <rajmund.zawislak@gmail.com>
2017-08-21 21:33:25 +0200

    Removed mistaken cd command

--- debian/generate-submodules.sh
+++ debian/generate-submodules.sh
@@ -5,7 +5,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../roles" || exit
 (
 	find  ../ -type f -name '*.yml' -print0 | xargs --null egrep --no-filename -e '^\s*-\s?role:\s?[^/]+$$' | sed --regexp-extended 's/.*?:\s+//'
 	test -r galaxy/requirements-manual.txt && grep --invert-match '^\s*#' galaxy/requirements-manual.txt
-	cd roles/
 ) | sort -u | sed 's#debops\.#ansible-#' | while read line ; do
    git submodule add https://github.com/debops/$line
 done
